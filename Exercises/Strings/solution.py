9b82cbef6554c75cddbbdfa40739dd1fc57aaf27
Riad De Pauw <riad.depauw@student.ucll.be>
2023-11-09 00:35:28 +0100

    Solution file prepared

---
+++ Exercises/Strings/solution.py
@@ -0,0 +1,38 @@
+#
+#        Feel free to make this your playground, have fun coding Python.
+#   
+#        Count sheep
+#        If you can't sleep, just count sheep!!
+#
+#        Task:
+#        Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+#Write ur code here:
+
+
+
+
+
+
+
+
+
+
+
+#        Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+#        
+#        Reverse words
+#        Examples
+#        "This is an example!" ==> "sihT si na !elpmaxe"
+#        "double  spaces"      ==> "elbuod  secaps"
+#Write ur code here:
+
+
+
+
+
+
+
+
+
+
+# Write ur code here:
