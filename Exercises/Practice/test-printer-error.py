b5545e10f38691b6f9e2bc859a20d0b6732b7c78
Riad De Pauw <riad.depauw@student.ucll.be>
2023-11-16 21:41:21 +0100

    Printer exercise added

---
+++ Exercises/Practice/test-printer-error.py
@@ -0,0 +1,13 @@
+import codewars_test as test
+from student import printer_error
+
+@test.describe("printer_error")
+def basic_tests():
+    @test.it('Example Test Cases')
+    def example_test_cases():
+        s="aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+        test.assert_equals(printer_error(s), "3/56")
+        s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"
+        test.assert_equals(printer_error(s), "6/60")
+        s = "kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu"
+        test.assert_equals(printer_error(s) , "11/65")
