aba99019f4e06648c39aef27f891f0fa5e8f78c0
Riad De Pauw <riad.depauw@student.ucll.be>
2023-12-16 22:41:09 +0100

    list filtering exercise added

--- Exercises/Practice/solution.py
+++ Exercises/Practice/solution.py
@@ -480,3 +480,24 @@ def remove_every_other(my_list):
 # list slicing is a way to extract a portion of a list.
 # list slicing is done by defining the start, stop, and step values.
 # list slicing is done by using the colon operator.
+
+
+
+
+
+
+# In this kata you will create a function that takes a list of non-negative integers 
+# and strings and returns a new list with the strings filtered out.
+
+# Example
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+# Write ur code here:
+
+def filter_list(l):
+    filter = []
+    for num in l:
+        if isinstance(num, int):
+            filter.append(num)
+    return filter
