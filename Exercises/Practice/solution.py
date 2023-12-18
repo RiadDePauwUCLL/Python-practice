6974757b427a8463d91d23cf610ed2296896590b
Riad De Pauw <riad.depauw@student.ucll.be>
2023-12-18 18:11:24 +0100

    Is it even exercise added

--- Exercises/Practice/solution.py
+++ Exercises/Practice/solution.py
@@ -532,3 +532,30 @@ def square_digits(num):
     for x in str(num):
         ret += str(int(x)**2)
     return int(ret)
+
+
+
+
+
+
+# In this Kata we are passing a number (n) into a function.
+
+# Your code will determine if the number passed is even (or not).
+
+# The function needs to return either a true or false.
+
+# Numbers may be positive or negative, integers or floats.
+
+# Floats with decimal part non equal to zero are considered UNeven for this kata.
+# Write ur code here:
+
+def is_even(n): 
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+# this makes it clearer on how it works. But you can also do it like this, just less readable:
+    
+def is_even(n): 
+    return n % 2 == 0
