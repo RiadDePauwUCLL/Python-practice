628d8475f21fe1a7ce9f369ff5e586279908641a
Riad De Pauw <riad.depauw@student.ucll.be>
2024-03-22 15:17:12 +0100

    nth term series exercise added

--- Exercises/Practice/solution.py
+++ Exercises/Practice/solution.py
@@ -1801,3 +1801,39 @@ def domain_name(url):
     stripped_url = url.replace("https://", "").replace("http://", "").replace("www.", "")
     domain_name = stripped_url.split(".")[0]
     return domain_name
+
+
+
+
+
+
+
+# Sum of the first nth term of Series
+
+# Your task is to write a function which returns the n-th term of the following series, 
+# which is the sum of the first n terms of the sequence (n is the input parameter).
+
+# You will need to figure out the rule of the series to complete this.
+
+# Rules
+# You need to round the answer to 2 decimal places and return it as String.
+
+# If the given value is 0 then it should return "0.00".
+
+# You will only be given Natural Numbers as arguments.
+
+# Examples (Input --> Output)
+# n
+# 1 --> 1 --> "1.00"
+# 2 --> 1 + 1/4 --> "1.25"
+# 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+# Write ur code here:
+
+def series_sum(n):
+    if n == 0:
+        return '0.00'
+    
+    else:
+        total = sum(1/(1 + 3*(i - 1)) for i in range(1, n + 1))
+        return f'{total:.2f}'
